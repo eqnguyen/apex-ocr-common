d1ad7cdd6babe17025bdf56ded912f780b220ca7
eqnguyen <eqnguyen@gmail.com>
2023-05-05 03:03:04 -0400

    Create script to remove duplicates in squad stats CSV based on hash

---
+++ scripts/remove_duplicates.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import click
+import pandas as pd
+
+
+@click.command()
+@click.argument(
+    "filepath",
+    required=True,
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+)
+def remove_duplicates(filepath: Path) -> None:
+    if filepath.exists() and filepath.suffix == ".csv":
+        results_df = pd.read_csv(filepath)
+        results_df = results_df.drop_duplicates(subset="Hash", keep="first")
+        results_df.to_csv(filepath, index=False)
+
+
+if __name__ == "__main__":
+    remove_duplicates()
