eeec43a7f66a58a704402c905c0cab5164ce19e7
eqnguyen <eqnguyen@gmail.com>
2023-06-28 00:37:38 -0400

    Create module for downloading screenshots from Google Drive

--- apex_ocr/config.py
+++ apex_ocr/config.py
@@ -7,12 +7,19 @@ logger = logging.getLogger(__name__)
 # Valid image extensions
 IMAGE_EXTENSIONS = [".jpg", ".jpeg", ".png"]
 
-# Path to stats file
+# Path to log directory
+LOG_DIRECTORY = Path(__file__).parent.parent / "logs"
+LOG_DIRECTORY.mkdir(parents=True, exist_ok=True)
+
+# Path to data
 DATA_DIRECTORY = Path(__file__).parent.parent / "data"
 DATA_DIRECTORY.mkdir(parents=True, exist_ok=True)
+
+GOOGLE_DRIVE_DIRECTORY = Path(__file__).parent.parent / "data" / "google_drive"
+GOOGLE_DRIVE_DIRECTORY.mkdir(parents=True, exist_ok=True)
 
 SQUAD_STATS_FILE = DATA_DIRECTORY / "squad_stats.csv"
 
 # Database output
-DATABASE = False
+DATABASE = True
 DATABASE_YML_FILE = Path(__file__).parent.parent / "db.yml"
